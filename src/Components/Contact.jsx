67d961e8f0ab3d8a2d53d93be923daebc27d4cb4
Ajit Vagoli <ajitvagoli34@gmail.com>
2025-04-11 16:07:31 +0530

    button color update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <h3>Name: Ajit Manohar Vagoli</h3>
     <h3>Mobile.No: +91 9284079473</h3>
     <h3>Gmail: ajitvagoli34@gmail.com</h3>
-    <button className='link' onClick={() => window.open("https://www.linkedin.com/in/ajit-vagoli-0a0455238", "_blank")}>
+    <button className='cbut' onClick={() => window.open("https://www.linkedin.com/in/ajit-vagoli-0a0455238", "_blank")}>
     Visit My LinkedIn
     </button>
 
