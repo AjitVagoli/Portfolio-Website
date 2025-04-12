ae23ad77b3f01cc7257c83516d5570aa0b7b8a49
Ajit Vagoli <ajitvagoli34@gmail.com>
2025-04-12 12:34:58 +0530

    contact update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='cont'>
-    <h3>Name: Ajit Manohar Vagoli</h3>
-    <h3>Mobile.No: +91 9284079473</h3>
-    <h3>Gmail: ajitvagoli34@gmail.com</h3>
+    <h3>Ajit Manohar Vagoli</h3>
+    <h3>+91 9284079473</h3>
+    <h3>ajitvagoli34@gmail.com</h3>
     <button className='cbut' onClick={() => window.open("https://www.linkedin.com/in/ajit-vagoli-0a0455238", "_blank")}>
     Visit My LinkedIn
     </button>
